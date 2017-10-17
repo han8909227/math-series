6c64e190fe5b86570a399b010d071703f52d7bdc
Megan Flood <mak.flood@comcast.net>
2017-10-17 13:50:06 -0700

    Added function to compute Lucas Numbers Added the computation function to series.py and a test for the function to test_series.py. Also edited the list of testing parameters for the fib series and automatically generated the lucas parameters.

--- series.py
+++ series.py
@@ -1,4 +1,5 @@
-"""Series functions"""
+"""Series functions."""
+
 
 def fibonacci(n):
     if n == 1:
@@ -7,5 +8,19 @@ def fibonacci(n):
     if n == 2:
         return 1
 
-    return fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
+
+def lucas(n):
+    """Find the nth value in the Lucas Numbers series."""
+    if n == 0:
+        return 2
+
+    if n == 1:
+        return 1
+
+    num1 = 2
+    num2 = 1
+    for _ in range(n - 1):
+        num1, num2 = num2, num1 + num2
+    return num2
