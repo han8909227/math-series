c471a239f319b538ef4dcf8b496bd6f27e882ee8
han8909227 <hbao2016@hotmail.com>
2017-10-17 14:25:34 -0700

    sum series function passes the test when the initial values are the same as fib or lucas

--- series.py
+++ series.py
@@ -1,17 +1,15 @@
-"""Series functions."""
-
+"""A group of sum series functions"""
 
 
 def fibonacci(n):
-    if n == 1:
+    """Find the nth value in the Fibonacci Series."""
+    if n == 0:
         return 0
 
-    if n == 2:
+    if n == 1:
         return 1
 
     return fibonacci(n - 1) + fibonacci(n - 2)
-<<<<<<< HEAD
-=======
 
 
 def lucas(n):
@@ -27,4 +25,15 @@ def lucas(n):
     for _ in range(n - 1):
         num1, num2 = num2, num1 + num2
     return num2
->>>>>>> c817527ea94d2032bf562c5043fe6b8d88ee67a5
+
+
+def sum_series(n, num1=0, num2=1):
+    """Find the nth value in a sum series
+    given 1st and 2nd number in that series"""
+    if n == 0:
+        return num1
+
+    if n == 1:
+        return num2
+
+    return sum_series((n - 1), num1, num2) + sum_series((n - 2), num1, num2)
